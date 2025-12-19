8604c87f2fabad01617571c5e024f8587b3f88ee
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-19 20:38:21 +0600

    addad new html file in js folder

--- script/login.js
+++ script/login.js
@@ -3,13 +3,28 @@ document.getElementById("btn-login")
     .addEventListener("click", function (event) {
         event.preventDefault();
         const accountNumber = document.getElementById("account-number").value;
-        if (accountNumber.length === 11) {
-            console.log("yes right")
-        }
-        else {
-            console.log("rong")
-        }
 
         const pin = document.getElementById("pin").value;
-        console.log(accountNumber, pin)
+        const convert = parseInt(pin)
+
+        // if (accountNumber.length === 11 && convert === 1234) {
+        //     console.log("ok you are okay")
+
+        // }
+        // else {
+        //     console.log("need valid account pin and number")
+        // }
+
+        // or
+
+        if (accountNumber.length === 11) {
+            if (convert === 1234) {
+                window.location.href = "./main.html"
+            } else {
+                console.log("incorrect your pin ")
+            }
+        } else {
+            console.log("need valid number")
+        }
+
     })
