f99887ed64942c2742ec4b765a30e6f628cc2cde
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-21 11:33:41 +0600

    addad click function if click add money. add money section is visible and hidencashout section if click to cashout div so visible cashout section and add money section headen

---
+++ script/toggle.js
@@ -0,0 +1,14 @@
+document.getElementById("cashout-section").style.display = "none"
+document.getElementById("add-money-div")
+    .addEventListener("click", function () {
+        document.getElementById("add-money-section").style.display = "block"
+        document.getElementById("cashout-section").style.display = "none"
+
+    })
+
+document.getElementById("cashout")
+    .addEventListener("click", function () {
+        document.getElementById("add-money-section").style.display = "none"
+        document.getElementById("cashout-section").style.display = "block"
+
+    })
